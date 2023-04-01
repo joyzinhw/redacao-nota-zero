a3706514ab86d24dba45329febc99d4b71051dcd
joyzinhw <joycemouraifpi@gmail.com>
2023-04-01 18:01:11 -0300

    extraçaõ de dados

---
+++ filtradordedados.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+
+tabela = pd.read_excel("essay-br.xlsx", engine='openpyxl')
+
+filtrados = tabela['score'] == 0
+
+print("dados filtrados:")
+print(tabela[filtrados])
+
+pd.get_option('display.max_rows')
+pd.set_option('display.max_row', 100)
+print("dados filtrados:")
+print(tabela[filtrados])
+
+tabela[filtrados].to_excel('filtrados.xlsx', index=False)
